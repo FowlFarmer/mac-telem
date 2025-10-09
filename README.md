2ea6310422708e71d6baace1eac26441c05706d5
Theodore <theodorez888@gmail.com>
2025-10-08 20:46:08 -0400

    doc changes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mac-telem
 Simple daemon that allows your mac to update a mongodb database with your battery, location and timestamp
+
+!! Put the repo in a non-protected folder. Apple protects folders such as Desktop, Downloads, etc where the venv cannot be accessed by a launchagent.
+- I suggest putting it in the ~ directory (home i.e. /Users/theodore ) or a custom subdir like ~/projects/
+
+1. Create a python venv in this dir, then source activate, and install requirements.txt
+
+2. Edit the plist and setup.sh to reflect your directories and move it into Library/Launchagents folder on mac
+
+3. Run setup.sh
--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 # Gotta add fileperms: go to settings, search for file access, then do cmd+shift+g to set absolute path, then add /usr/bin/python3 
 # edit theodore to whatever ur mac home name is and stick the plist in ~/Library/LaunchAgents/
 
-/usr/bin/python3 -m pip install --user pymongo psutil pyobjc python-dotenv
+# Make sure to set up the venv in this repo initally with requirements.txt
+
+rm -rf ~/Library/Logs/telemetry.stderr.log
 launchctl unload ~/Library/LaunchAgents/com.theodore.telemetry.plist 2>/dev/null || true
 launchctl load   ~/Library/LaunchAgents/com.theodore.telemetry.plist
 launchctl start  com.theodore.telemetry
